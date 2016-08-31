9edb1a8055ff182808fa34ebb24bbf838e42e6f9
Jake Hoffmann <jakeehoffmann@gmail.com>
2016-08-30 21:56:13 -0400

    more incremental changes and testing

--- public/app.js
+++ public/app.js
@@ -68,11 +68,10 @@ myApp.factory('Auth', ['$location', function($location) {
             }
         },
         auth: function () {
-            console.log(encodeURI('http://hoffmannbot.herokuapp.com/#/hoffmannbot/get/'));
-            var client_id='49mrp5ljn2nj44sx1czezi44ql151h2',
+                var client_id='49mrp5ljn2nj44sx1czezi44ql151h2',
                 force_verify='true',
                 scope='channel_editor',
-                redirect_uri=encodeURI('http://hoffmannbot.herokuapp.com/#/hoffmannbot/get/'),
+                redirect_uri='http%3A%2F%2Fhoffmannbot.herokuapp.com%2F%23%2Fhoffmannbot%2Fget%2F',
                 response_type='code',
                 url='https://api.twitch.tv/kraken/oauth2/authorize?response_type='+response_type+'&force_verify='+force_verify+'&client_id='+client_id+'&redirect_uri='+redirect_uri+'&scope='+scope;
             window.location.replace(url); 
