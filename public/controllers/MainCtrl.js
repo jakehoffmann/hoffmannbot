9591957a4e5c397fbfa6ca594a13704a718b1ac0
Jake Hoffmann <jakeehoffmann@gmail.com>
2016-10-04 05:39:06 -0400

    -

--- public/controllers/MainCtrl.js
+++ public/controllers/MainCtrl.js
@@ -2,12 +2,23 @@
 myApp.controller('MainCtrl', ['$scope', '$http', '$uibModal', 'auth',
                               function ($scope, $http, $uibModal, auth) {
     $scope.checkAuth = auth.isAuthed; // Unsure if I am using this in the view
-    $scope.screenshots = [{src: '/images/lastcommand.png',
-                           desc: 'Displays stats from your last game in chat'
+    $scope.screenshots = [{
+                              src: '/images/lastcommand.png',
+                              desc: 'Displays stats from your last game in chat'
                           }, 
-                          {src: '/images/titleupdate.png',
-                           desc: 'Updates your title with your in-game info'
-                          }] 
+                          {
+                              src: '/images/titleupdate.png',
+                              desc: 'Updates your title with your in-game info'
+                          },
+                          {
+                              src: '/images/rankcommand.png',
+                              desc: 'Displays the ranks of all your registered accounts'
+                          },
+                          {
+                              src: '/images/currentcommand.png',
+                              desc: 'Shows what you are currently playing, as well as (optionally) your runes and the bans for the game'
+                          }
+                          ] 
     
     $scope.openModal = function(src) {
         $uibModal.open({
